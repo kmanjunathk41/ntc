890b6902dad1a47665e5a20a0d7fe24aea1eca9c
manjuk41 <manjunath.k41@gmail.com>
2018-01-14 21:51:54 +0530

    cons

--- nuxtServer.js
+++ nuxtServer.js
@@ -1,7 +1,7 @@
 import Koa from 'koa';
 import {Nuxt, Builder} from 'nuxt';
 import {connectMongoose} from "./nodeserver/mongoose"
-import {router} from "./nodeserver/routeHandler"
+// import {router} from "./nodeserver/routeHandler"
 const app = new Koa();
 const host = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 7616;
@@ -16,6 +16,14 @@ const nuxt = new Nuxt(config);
 
 //connect mongoose
 connectMongoose();
+
+var Router = require('koa-router');
+var router = new Router();
+
+router.get('/userLogin', (ctx, next) => {
+  console.log("user login route")
+  ctx.body = {login:'login available'};
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
